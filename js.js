8c2a9dfa5ea9146b269b84865bb821b14d67a12e
Unknown <wow1ghzx@gmail.com>
2018-04-10 11:18:24 +0300

    arrayt ja toimivat päivät

--- js.js
+++ js.js
@@ -1,15 +1,77 @@
-	var luukut = []
+	var luukut = [
+	{
+		"title" : "getFullYear1",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+	{
+		"title" : "getFullYear2",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+	{
+		"title" : "getFullYear3",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+	{
+		"title" : "getFullYear4",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear5",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear6",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear7",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear8",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear9",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear10",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear11",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear13",
+		"description" : "7iyr",
+		"image" : "image.image"
+	},
+		{
+		"title" : "getFullYear23",
+		"description" : "7iyr",
+		"image" : "image.image"
+	}
+
+	]
 
 /*tähän tulee Array(eli kuvat)*/
 
-	function culli() {
-	var da = new Date();
-	var te = da.getDate();
-	console.log(te);
-	}
 
 
-	function week() { /*tää hakee nykyisen viikon*/
+	/*tää hakee nykyisen viikon*/
 		Date.prototype.getWeekNumber = function(){
 		var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
 		var dayNum = d.getUTCDay() || 7;
@@ -18,79 +80,14 @@
 		var outputs = Math.ceil((((d - yearStart) / 86400000) + 1)/7);
 		return outputs; /*kuinka saada viikkonumero? 'new Date().getWeekNumber()' palauttaa nykyisen viikon*/
 		}
-	}
+	
 
 	function testis() {
-	console.log(new Date().getWeekNumber());
-	var currentviikko = new Date().getWeekNumber();
 
-
-		for (var i = Things.length - 1; i >= 0; i--) {
-			Things[i]
+	/*var currentviikko = (new Date().getWeekNumber());  tätä käytetään oikeessa versiossa*/
+	var currentviikko = 13; /*nyt vaan laitetaan viikko numeroon 13 testauksen vuoks*/
+		for (var i = luukut.length; i >= currentviikko; i--) /*<--vituttaa tällänen*/{
+			luukut[i]
 		}
-	}
-
-	function function_name(argument) {
-		// body...
-	}
-
-
-
-
-
-
-
-
-
-
-var myCal = document.getElementById("adventCal");
-var currentDate = new Date();
-
-function Door(calendar, day) {
-
-	this.width = ((calendar.width - 0.1 * calendar.width) / 4) * 0.95;
-	this.height = ((calendar.height - 0.1 * calendar.height) / 6) * 0.95;
-	this.adventMessage = 'Day ' + day + ' of Advent\n\n' + '"' + messages[day - 1][0] + '"\n\n\t' + 'by ' + messages[day - 1][1] + '\n\n';
-	this.x = ( 0.04 * calendar.width + ((day- 1) % 4) * (1.1 * this.width) );
-	this.y = - ( 0.96 * calendar.height - Math.floor((day - 1) / 4) * (1.1 * this.height) );
-
-	this.content = function() { 
-		
-		var node = document.createElement("li");
-		document.getElementById("adventDoors").appendChild(node);
-		node.id = "door" + day;
-		node.style.cssText = "width: " + this.width + "px; height: " + this.height + "px; top: " + this.y + "px; left: " + this.x + "px;";
-
-		var innerNode = document.createElement("a");
-		document.getElementById("door" + day).appendChild(innerNode);
-		innerNode.innerHTML = day;
-		innerNode.href = "#";
-
-		if( ( currentDate.getMonth() + 1 ) < 12 || currentDate.getDate() < day ) {
-			innerNode.className = "disabled";
-			innerNode.onclick = function() {
-				return false;
-			}
-		} else {
-			var adventMessage = this.adventMessage;
-			innerNode.onclick = function() {
-				alert(adventMessage);
-				return false;
-			}
-		}	
-	};
-
-}
-
-(function() {
-	var doors = [];
-
-	for(var i = 0; i < 24; i++) {
-
-		doors[i] = new Door(myCal, i + 1);
-		doors[i].content();
 
 	}
-
-	return doors;
-})();
