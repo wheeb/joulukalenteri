fd27476764a424d52d9be11e28e15c0eecf70a2b
teemuhirvonen <38240016+teemuhirvonen@users.noreply.github.com>
2018-04-10 09:54:55 +0300

    uj

--- js.js
+++ js.js
@@ -9,5 +9,6 @@
 		var dayNum = d.getUTCDay() || 7;
 		d.setUTCDate(d.getUTCDate() + 4 - dayNum);
 		var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-		return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
-};
+		return Math.ceil((((d - yearStart) / 86400000) + 1)/7);
+		}
+	}
