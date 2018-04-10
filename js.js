205b182d24bd08a208caa7678435df6b8fe161ad
Unknown <wow1ghzx@gmail.com>
2018-04-10 09:48:38 +0300

    sexxx

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
-function paskahomo() {
-	// body...
-}
-
-function {
+	function culli() {
 	var da = new Date();
 	var te = da.getDate();
 	console.log(te);
-}
+	}
+	function week() {
+		Date.prototype.getWeekNumber = function(){
+		var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+		var dayNum = d.getUTCDay() || 7;
+		d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+		var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+		return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+};
