ad6e4f3c1254101c97f421e2cd4ec250271d0ee7
Unknown <wow1ghzx@gmail.com>
2018-04-10 10:27:30 +0300

    luukut.png

--- js.js
+++ js.js
@@ -1,14 +1,96 @@
+	var luukut = []
+
+/*tähän tulee Array(eli kuvat)*/
+
 	function culli() {
 	var da = new Date();
 	var te = da.getDate();
 	console.log(te);
 	}
-	function week() {
+
+
+	function week() { /*tää hakee nykyisen viikon*/
 		Date.prototype.getWeekNumber = function(){
 		var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
 		var dayNum = d.getUTCDay() || 7;
 		d.setUTCDate(d.getUTCDate() + 4 - dayNum);
 		var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-		return Math.ceil((((d - yearStart) / 86400000) + 1)/7);
+		var outputs = Math.ceil((((d - yearStart) / 86400000) + 1)/7);
+		return outputs; /*kuinka saada viikkonumero? 'new Date().getWeekNumber()' palauttaa nykyisen viikon*/
 		}
 	}
+
+	function testis() {
+	console.log(new Date().getWeekNumber());
+	var currentviikko = new Date().getWeekNumber();
+
+
+		for (var i = Things.length - 1; i >= 0; i--) {
+			Things[i]
+		}
+	}
+
+	function function_name(argument) {
+		// body...
+	}
+
+
+
+
+
+
+
+
+
+
+var myCal = document.getElementById("adventCal");
+var currentDate = new Date();
+
+function Door(calendar, day) {
+
+	this.width = ((calendar.width - 0.1 * calendar.width) / 4) * 0.95;
+	this.height = ((calendar.height - 0.1 * calendar.height) / 6) * 0.95;
+	this.adventMessage = 'Day ' + day + ' of Advent\n\n' + '"' + messages[day - 1][0] + '"\n\n\t' + 'by ' + messages[day - 1][1] + '\n\n';
+	this.x = ( 0.04 * calendar.width + ((day- 1) % 4) * (1.1 * this.width) );
+	this.y = - ( 0.96 * calendar.height - Math.floor((day - 1) / 4) * (1.1 * this.height) );
+
+	this.content = function() { 
+		
+		var node = document.createElement("li");
+		document.getElementById("adventDoors").appendChild(node);
+		node.id = "door" + day;
+		node.style.cssText = "width: " + this.width + "px; height: " + this.height + "px; top: " + this.y + "px; left: " + this.x + "px;";
+
+		var innerNode = document.createElement("a");
+		document.getElementById("door" + day).appendChild(innerNode);
+		innerNode.innerHTML = day;
+		innerNode.href = "#";
+
+		if( ( currentDate.getMonth() + 1 ) < 12 || currentDate.getDate() < day ) {
+			innerNode.className = "disabled";
+			innerNode.onclick = function() {
+				return false;
+			}
+		} else {
+			var adventMessage = this.adventMessage;
+			innerNode.onclick = function() {
+				alert(adventMessage);
+				return false;
+			}
+		}	
+	};
+
+}
+
+(function() {
+	var doors = [];
+
+	for(var i = 0; i < 24; i++) {
+
+		doors[i] = new Door(myCal, i + 1);
+		doors[i].content();
+
+	}
+
+	return doors;
+})();
